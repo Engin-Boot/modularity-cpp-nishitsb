c427ae9deaf60819ac8081f70c6244a09ba53746
nishitsb <nishitsbansal1997@gmail.com>
2020-08-24 22:56:55 +0530

    Modularity and new feature done

---
+++ ColorCoding.h
@@ -0,0 +1,6 @@
+#ifndef COLOR_CODING_H
+#define COLOR_CODING_H
+
+void colorCodingManual();
+
+#endif
---
+++ ColorPair.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+namespace TelCoColorCoder
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+
+    ColorPair GetColorFromPairNumber(int pairNumber);
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
+
+#endif
